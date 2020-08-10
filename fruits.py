e1ca2681aeb74bf451f34a4dee47eab2637d3792
Pigrabbit <donghyuk1003@gmail.com>
2020-08-10 21:22:22 +0900

    Refactor crawling page code

--- fruits.py
+++ fruits.py
@@ -1,14 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-def getPageString(uri):
-    data = requests.get(uri, headers={
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"
-    })
+USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"
+HEADER = {'User-Agent': USER_AGENT}
+URI = "https://www.coupang.com/np/categories/194282?page="
+
+def get_page_bs_obj(uri):
+    data = requests.get(uri, headers=HEADER)
     bs_obj = BeautifulSoup(data.content, "html.parser")
+    return bs_obj
 
-    print(bs_obj.find_all('ul', attrs={"class":"baby-product-list"}))
-    return data.content
+def get_product_list(bs_obj):
+    return bs_obj.find('ul', attrs={"class":"baby-product-list"})
 
-uri = "https://www.coupang.com/np/categories/194282?page=1"
-getPageString(uri)
+def get_product_items(product_list_obj):
+    return product_list_obj.find_all('li', attrs={"class": "baby-product"})
+
+def crawl_single_page(idx):
+    page_uri = URI + str(idx)
+    bs_obj = get_page_bs_obj(page_uri)
+    product_list = get_product_list(bs_obj)
+    prodcut_items = get_product_items(product_list)
+    print(len(prodcut_items))
+
+    return []
+
+idx = list(range(1, 11))
+
+for i in idx:
+    crawl_single_page(i)
